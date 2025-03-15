ef2859413cf5085e4cc983669fd72d5af50e8f24
user4857535 <spillerdev@gmail.com>
2025-03-15 08:29:42 +0545

    adding h1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,6 +32,7 @@ export default function Home() {
                 <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
                 Deploy now
           </a>
+                    <h1>hello</h1>
           <a className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]" href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer" >
                 Read our docs
           </a>
